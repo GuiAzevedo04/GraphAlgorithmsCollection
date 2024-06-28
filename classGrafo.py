e85f36406c16ab26b59ce03ac2c51d92f237d365
Guilherme Luiz <guilhermeazevedo2212@gmail.com>
2024-06-28 14:05:44 -0300

    Pequenas mudancas de identação

--- classGrafo.py
+++ classGrafo.py
@@ -1,46 +1,49 @@
-#imports ReGex modules
+#Importa o modulo de ReGex
 import re
 
 class Grafo:
     def __init__(self):
         pass
     
-    #função que adiciona um atributo na classe
+    #Função que adiciona um atributo na classe
     def adicionar_atributo(self, nome_atributo, valor) -> None: 
-        setattr(self, nome_atributo, valor)     #é só adicionar o nome do atributo e seu valor
+        setattr(self, nome_atributo, valor)
 
-    #identifica se o padrão do txt é válido
-    def graphPattern(self, string) -> bool: 
-        test = r"^\s*V\s*=\s*{[0-9]?[,0-9]*};\s*A\s*=\s*{[\(0-9,0-9\)]?[,\(0-9,0-9\)]*};$" #regex do grafo
+    #Identifica se o padrão do .txt é válido
+    def graph_pattern(self, string) -> bool: 
+        test = r"^\s*V\s*=\s*{[0-9]?[,0-9]*};\s*A\s*=\s*{[\(0-9,0-9\)]?[,\(0-9,0-9\)]*};$" #ReGex do grafo
         result = re.match(test, string)
         return bool(result)
     
-    #lê o conteúdo do .txt e retorna seu conteúdo
-    def readtxt(self) -> str: 
-        nameArq = input("insira o nome do arquivo que contém o grafo: ")
-        graphArq = open(nameArq, "r")
-        content = graphArq.read()
+    #Lê o conteúdo do .txt e retorna seu conteúdo
+    def read_txt(self) -> str: 
+        name_arq = input("insira o nome do arquivo que contém o grafo: ")
+        graph_arq = open(name_arq, "r")
+        content = graph_arq.read()
 
-        if(self.graphPattern(content)):
+        if(self.graph_pattern(content)):
             return content
         else:
             print("O padrão do arquivo está incorreto!")    
     
-    #lê a string e produz os vértices e arestas
-    def readGraphString(self, graphString) -> None: 
-        vertContent = graphString.split('V = ')[1].split(';')[0].strip() #separa em 2 partes usando "V = ", e pega a segunda parte.
-        vertContent = vertContent.strip("{}")                            #separa em 2 partes de novo, desta vez com ";", e pega a primeira parte
-        vertContent = vertContent.split(",")                             #por fim, elimina os espaços em branco em volta da string
+    #Lê a string e produz os vértices e arestas
+    def read_graph_string(self, graph_string) -> None: 
+        vert_content = graph_string.split('V = ')[1].split(';')[0].strip() #Separa em 2 partes usando "V = ", e pega a segunda parte.
+        vert_content = vert_content.strip("{}")                            #Separa em 2 partes de novo, desta vez com ";", e pega a primeira parte
+        vert_content = vert_content.split(",")                             #Por fim, elimina os espaços em branco em volta da string
         
-        self.adicionar_atributo('vertices', vertContent)                 #cria um novo atributo chamado 'vertices'
+        #Cria um novo atributo chamado 'vertices'
+        self.adicionar_atributo('vertices', vert_content)                 
 
-        edgeContent = graphString.split('A = ')[1].split(';')[0].strip()
-        edgeContent = edgeContent.strip("{}")
-        edgeArray = []
-        for x in edgeContent.split('),('): #divide edgeContent por '),('
+        edge_content = graph_string.split('A = ')[1].split(';')[0].strip()
+        edge_content = edge_content.strip("{}")
+        edge_array = []
+
+        #Divide edge_content por '),('
+        for x in edge_content.split('),('): 
             x = x.strip("()")
-            u, v = x.split(',')            #guarda cada membro da aresta em u e v
-            edgeArray.append((u,v))        #guarda u e v na lista
+            u, v = x.split(',')         #Guarda cada membro da aresta em u e v
+            edge_array.append((u,v))    #Guarda u e v na lista
 
-        self.adicionar_atributo('arestas', edgeArray) #cria um novo atributo chamado 'arestas'
-
+        #Cria um novo atributo chamado 'arestas'
+        self.adicionar_atributo('arestas', edge_array) 
