e16a60cef40d62922cf747bf4e46ba4e7af773c2
Guilherme Luiz <guilhermeazevedo2212@gmail.com>
2024-06-28 18:58:03 -0300

    Started the basics of the user interface

--- Main.py
+++ Main.py
@@ -1,13 +1,50 @@
 from Graph import *
 
-is_directed = bool(input("Is the graph directed? [1] Yes | [0] No:\n"))
+def interface_text() -> None:
+    print(f"[0] Read new .txt file: ")
+    print(f"[1] Make Verifications")
+    print(f"[2] List components")
+    print(f"[3] Generate Adjacency matrix")
+    print(f"[4] Generate Adjacency list")
+    print(f"[5] Generate Depth Tree")
+    print(f"[6] Generate Minimum spanning tree")
+    print(f"[7] Topological sort (This function is not available in undirected graphs)")
+    print(f"[8] Shortest path between two vertices (This function is not available in unweighted graphs)")
+    print(f"[9] Transitive closure (This function is not available in unweighted graphs): ")
+    print(f"[C] Close program.")
 
-my_graph = Graph(is_directed)
 
-my_graph.read_graph_string()
 
-print(my_graph.edges)
-print(my_graph.vertices)
+def main():
+    user_input = str
+    close_program = False
+    while not close_program:
+        
+        interface_text()
+        user_input = input("").lower()
 
-print(my_graph.edges[2])
-print(my_graph.vertices[1])
+        match user_input:
+            case '0':
+                print("ToDo")
+            case '1':
+                print("ToDo")
+            case '2':
+                print("ToDo")
+            case '3':
+                print("ToDo")
+            case '4':
+                print("ToDo")
+            case '5':
+                print("ToDo")
+            case '6':
+                print("ToDo")
+            case '7':
+                print("ToDo")
+            case '8':
+                print("ToDo")
+            case '9':
+                print("ToDo")
+            case 'c':
+                close_program = True
+            case _:
+                print("Invalid Command")
